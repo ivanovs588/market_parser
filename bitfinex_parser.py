ab5a96cccbbd619a72eeb4dc3a04e5417fc1a490
Ivan Ovsyannikov <32175074+ivanovs588@users.noreply.github.com>
2021-11-15 16:43:04 +0300

    Create bitfinex_parser.py

---
+++ bitfinex_parser.py
@@ -0,0 +1,96 @@
+import os
+import sys
+import time
+from collections import OrderedDict
+sys.path.append('../../../')
+
+from bfxapi import Client
+
+bfx = Client(
+  manageOrderBooks=True
+)
+# bfx = Client(
+#   logLevel='INFO'
+# )
+
+# @bfx.ws.on('error')
+# def log_error(err):
+#   print ("Error: {}".format(err))
+
+# @bfx.ws.on('status_update')
+# def log_msg(msg):
+#   print (msg)
+
+# async def start():
+#   await bfx.ws.subscribe_derivative_status('tBTCF0:USTF0')
+
+bfx.ws.on('connected', start)
+bfx.ws.run()
+class OrderBook:
+  def __init__(self, snapshot):
+    self.bids = OrderedDict()
+    self.asks = OrderedDict()
+    self.load(snapshot)
+
+  def load(self, snapshot):
+    for record in snapshot:
+      if record[2] >= 0:
+        self.bids[record[0]] = {
+          'count': record[1],
+          'amount': record[2]
+        }
+      else:
+        self.asks[record[0]] = {
+          'count': record[1],
+          'amount': record[2]
+        }
+
+  def update(self, record):
+    
+    if record[1] == 0:
+      if record[2] == 1:
+        
+        del self.bids[record[0]]
+      elif record[2] == -1:
+        
+        del self.asks[record[0]]
+    elif record[1] > 0:
+      if record[2] > 0:
+        
+        if record[0] not in self.bids:
+          self.bids[record[0]] = {}
+        self.bids[record[0]]['count'] = record[1]
+        self.bids[record[0]]['amount'] = record[2]
+      elif record[2] < 0:
+        
+        if record[0] not in self.asks:
+          self.asks[record[0]] = {}
+        self.asks[record[0]]['count'] = record[1]
+        self.asks[record[0]]['amount'] = record[2]
+
+obs = {}
+
+@bfx.ws.on('error')
+def log_error(err):
+  print ("Error: {}".format(err))
+
+@bfx.ws.on('order_book_update')
+def log_update(data):
+  obs[data['symbol']].update(data['data'])
+
+@bfx.ws.on('order_book_snapshot')
+def log_snapshot(data):
+  obs[data['symbol']] = OrderBook(data['data'])
+
+async def start():
+  await bfx.ws.subscribe('book', 'tBTCUSD')
+
+bfx.ws.on('connected', start)
+bfx.ws.run()
+
+for n in range(0, 10):
+  time.sleep(2)
+  for key in obs:
+    print(f"Printing {key} orderbook...")
+    print(f"{obs[key].bids}\n")
+    print(f"{obs[key].asks}\n")
